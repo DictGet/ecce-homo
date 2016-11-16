60e1718a89c642be622f7c94439c5b4bb3e815ae
Mike Thomas <mike.thomas@telefonica.com>
2016-11-16 12:16:44 +0100

    add fields for validation of arguments

---
+++ fields.py
@@ -0,0 +1,8 @@
+from webargs import fields
+from settings import RESIZE_METHODS
+
+image_args = {
+    'w': fields.Int(validate=lambda val: val > 1),
+    'h': fields.Int(validate=lambda val: val > 1),
+    't': fields.Str(validate=lambda val: val in RESIZE_METHODS)
+}
